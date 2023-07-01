2897dda2e2555eec3308d0a09a9b3edf3b5b0b6c
topzy2000 <topzy2000@yahoo.com>
2023-07-01 00:16:27 -0500

    Update sshd.pp

--- site/profile/manifests/sshd.pp
+++ site/profile/manifests/sshd.pp
@@ -86,7 +86,7 @@ class profile::sshd {
         owner  => 'root',
         group  => 'root',
         mode   => '0640',
-        source => 'puppet:///modules/sshd',
+        source => 'puppet:///modules/oye/sshd',
         notify => Service['sshd'],
       }
     }

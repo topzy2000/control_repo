b82d589c71d8951a7b8320a5211573c4f0757b2b
topzy2000 <topzy2000@yahoo.com>
2023-07-17 17:19:46 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ node default {
 node 'puppetmaster02.temioye.com' {
   include role::master_server
 }
-#node /^node/ {
- # include role::app_server
-#}
+node /^node/ {
+  include role::app_server
+}
 #hiera_include('classes')
